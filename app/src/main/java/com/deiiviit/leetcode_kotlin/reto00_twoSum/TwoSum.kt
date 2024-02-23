fa52754eadcc9e76d38cf694712d0f46fff7eccd
deivitdev <saladavid0110@gmail.com>
2024-02-23 10:13:42 -0300

    feat(add): twoSum done

---
+++ app/src/main/java/com/deiiviit/leetcode_kotlin/reto00_twoSum/TwoSum.kt
@@ -0,0 +1,23 @@
+package com.deiiviit.leetcode_kotlin.reto00_twoSum
+
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
+        }
+        return intArrayOf()
+    }
+}
+
+fun main(){
+val twoSum = TwoSum()
+    val nums = intArrayOf(2, 7, 11, 15)
+    val target = 9
+    val result = twoSum.twoSum(nums, target)
+    println(result.contentToString())
+}
